2934a7d8eeaa1ac3fb8fb5552ffedaaf61466a94
Benjy Cui <benjytrys@gmail.com>
2016-11-25 17:33:59 +0800

    feat: improve UX

--- scripts/generate.js
+++ scripts/generate.js
@@ -10,7 +10,9 @@ const currentURLMap = Object.assign({}, global.URLMap);
 const URLs = process.argv.slice(2);
 URLs.forEach(url => {
   const md5 = crypto.createHash('md5').update(url).digest('hex');
-  currentURLMap[`/${md5.slice(0, 7)}`] = url;
+  const shortenedURL = `/${md5.slice(0, 7)}`;
+  currentURLMap[shortenedURL] = url;
+  console.log(`${url} ==shorten=> http://u.ant.design${shortenedURL}`);
 });
 
 fs.writeFileSync(
@@ -18,4 +20,4 @@ fs.writeFileSync(
   `(typeof window !== 'undefined' ? window : global).URLMap = ${JSON.stringify(currentURLMap, null, 2)};`
 );
 
-console.log('URLs had been shortened, please commit and push it to GitHub!');
+console.log('\nURLs had been shortened, please commit and push it to GitHub!');
