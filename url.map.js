29c9051d815992c792bd7cf7e70df9bf72a45c16
Benjy Cui <benjytrys@gmail.com>
2016-11-28 11:13:16 +0800

    chore: update

--- url.map.js
+++ url.map.js
@@ -1,3 +1,4 @@
 (typeof window !== 'undefined' ? window : global).URLMap = {
   "/": "https://ant.design/",
+  "/SQEdMma": "https://ant.design/components/date-picker/#components-date-picker-demo-format"
 };
