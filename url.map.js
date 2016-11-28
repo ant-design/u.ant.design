44c829fcf34c2848ec216464a645dfd44b9559cc
Benjy Cui <benjytrys@gmail.com>
2016-11-28 11:36:08 +0800

    chore: update

--- url.map.js
+++ url.map.js
@@ -1,5 +1,6 @@
 (typeof window !== 'undefined' ? window : global).URLMap = {
   "/": "https://ant.design/",
   "/SQEdMma": "https://ant.design/components/date-picker/#components-date-picker-demo-format",
-  "/12iVT/s": "https://ant.design/components/time-picker/#components-time-picker-demo-size"
+  "/12iVT/s": "https://ant.design/components/time-picker/#components-time-picker-demo-size",
+  "/jI6pSlR": "https://ant.design/components/calendar/#components-calendar-demo-custom-render"
 };
