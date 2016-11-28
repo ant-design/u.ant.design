21aa313cf706016cec9374336b0acfb3d3d8cdaf
Benjy Cui <benjytrys@gmail.com>
2016-11-28 14:28:26 +0800

    chore: update

--- url.map.js
+++ url.map.js
@@ -5,5 +5,6 @@
   "/jI6pSlR": "https://ant.design/components/calendar/#components-calendar-demo-custom-render",
   "/TqQBDmm": "https://ant.design/components/breadcrumb/#API",
   "/Z4DAgKB": "https://ant.design/components/menu/#components-menu-demo-sider-current",
-  "/sPWcgxe": "https://ant.design/components/table/#components-table-demo-fixed-columns"
+  "/sPWcgxe": "https://ant.design/components/table/#components-table-demo-fixed-columns",
+  "/aAIpclA": "https://ant.design/components/date-picker/#components-date-picker-demo-start-end"
 };
