1223d4f2bc406da47c9ad6a5a84569759060c4b0
Wei Zhu <yesmeck@gmail.com>
2018-08-10 11:09:41 +0800

    Update url.map.js

--- url.map.js
+++ url.map.js
@@ -12,5 +12,6 @@
   "/popover-content": "https://ant.design/components/popover/#components-popover-demo-basic",
   "/get-field-decorator": "https://ant.design/components/form/#components-form-demo-horizontal-login",
   "/gg2moment": "https://github.com/ant-design/ant-design/commit/4026221d451b246956983bb42140142d4a48b7d7",
-  "/codesandbox-repro": "https://codesandbox.io/api/v1/sandboxes/define?parameters=N4IgZglgNgpgziAXKADgQwMYGs0HMYB0AVnAPYB2SoGFALjObVSACYwoNvkYTzMBOMTE0QgoaenCYAaEIOEBaFqQC2SMRPgyQaRi3XjJ2laoYiQAJhABfW7Ijk2AD2IJkIGozPqIKlKX5aAAIAJSEMYLB-VSCAcnkI2IBuAB1yX39A0PDaABEAeQBZIKiY-JylVWS0jIDgkxUzEuiVOIazavS_OqDgINzNAAUIbBh-aSCANzG4CAog62ay3VoWTtqs2JWWAHoWCCkd7YIMODhOtLDhAsKCQUcxgAo0oKCAHn3JoKkAT1gAXmAfRUaH4uAciCCFgALAtrAA-F6vd4ob60P4wQHA0Hg8gAIVItFoqkhMLhiPIyORAGEAK78e7BbZTGZzciQ4DTfizCiLN4AI34OwpVNeAE1SLSghhdNKABa6fBBZlcnmU-a0OUwIKwMDBdDkGBQIKPXLsTgMHjwb4wCJsgCUBCRrzeOxQIuRbwG9GGo34QTYYDQtKgtAAamgoLTMcB2oxHvbFsLkUjXZ94dIkcoMLTGowCPhaABRWB52h4n4ASRYj3ipEJsXtaXtSRs9geLjltBUUGYnnojHUHwgXwgLH-KTk9dok_haZH8JstiAA",
+  "/codesandbox-repro": "https://codesandbox.io/s/wk04r016q8",
+  "/codesandbox": "https://codesandbox.io/s/wk04r016q8",
 };
