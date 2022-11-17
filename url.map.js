5d236e3bfc87f7f9d9995527c7bd843401907e53
afc163 <afc163@gmail.com>
2022-11-17 23:27:24 +0800

    Update url.map.js (#8)

--- url.map.js
+++ url.map.js
@@ -15,8 +15,10 @@
   "/complex-form-item": "https://ant.design/components/form/#components-form-demo-complex-form-control",
   "/form-deps": "https://ant.design/components/form/#dependencies/",
   "/gg2moment": "https://github.com/ant-design/ant-design/commit/4026221d451b246956983bb42140142d4a48b7d7",
-  "/codesandbox-repro": "https://codesandbox.io/s/antd-reproduction-template-y9vgcf",
-  "/codesandbox": "https://codesandbox.io/s/antd-reproduction-template-y9vgcf",
+  "/codesandbox": "https://codesandbox.io/s/antd-reproduction-template-forked-jyh2k9",
+  "/codesandbox-repro": "https://codesandbox.io/s/antd-reproduction-template-forked-jyh2k9",
+  "/codesandbox-4x": "https://codesandbox.io/s/antd-reproduction-template-y9vgcf",
+  "/codesandbox-repro-4x": "https://codesandbox.io/s/antd-reproduction-template-y9vgcf",
   "/pro-v2-default-settings": "https://github.com/ant-design/ant-design-pro/tree/master/src/defaultSettings.js",
   "/faq": "https://github.com/ant-design/ant-design/issues?q=is%3Aissue+label%3A%E2%9D%93FAQ+is%3Aclosed",
 };
